85839497a28eaf72bb4ebfef3004c96f1d2e9590
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-12-01 22:35:28 -0500

    Refactor VM index to consolidate implementation exports under code-execution, removing platform-specific exports for cleaner module structure.

--- lib/vm/index.ts
+++ lib/vm/index.ts
@@ -5,10 +5,5 @@
 export * from './types';
 export * from './base';
 export * from './manager';
-export * from './implementations/linux';
-export * from './implementations/windows';
-export * from './implementations/android';
-export * from './implementations/dos';
-export * from './implementations/playstation';
-export * from './implementations/xbox';
+export * from './implementations/code-execution';
 
